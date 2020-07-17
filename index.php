cdfa89a94238a484b12c0e098597e10f1a3c5cb6
Panos iliopoulos <piliop@gmail.com>
2020-07-18 02:17:06 +0300

    Add autofocus attribute

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
                 <div class="row mt-5">
                   <div class="col-12 col-md-10">
                     <div class="form-group">
-                      <input type="text" class="form-control" id="url" name="url" tab-index="0">
+                      <input type="text" class="form-control" id="url" name="url" tab-index="0" autofocus>
                     </div>                
                   </div>
                   <div class="col-12 col-md-2">
